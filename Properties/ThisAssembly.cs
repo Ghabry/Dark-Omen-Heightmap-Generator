a48f89397aa865b13dcd16c1694e655218642cf6
Ghabry <gabelk@web.de>
2012-11-19 00:58:36 +0100

    Update AssemblyInfo.

---
+++ Properties/ThisAssembly.cs
@@ -0,0 +1,11 @@
+﻿static class ThisAssembly
+{
+    internal const string Title = "HeightMapGenerator.exe";
+    internal const string Product = "Warhammer Dark Omen Heightmap Generator";
+    internal const string Description = "Heightmap Generator for the game Warhammer Dark Omen";
+    internal const string Copyright = "Copyright (C) 2012 Gabriel Kind";
+    internal const string Author = "Gabriel Kind";
+    internal const int Year = 2012;
+    internal const string Version = "2.0.0.0";
+    internal const string InformationalVersion = "2.0";
+}
