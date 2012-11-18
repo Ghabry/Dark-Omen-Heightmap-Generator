b2e1f4e1f9217ddf004b95c02593d9b04580a00a
Ghabry <gabelk@web.de>
2012-11-18 13:52:56 +0100

    Add function to convert Terr class into TERR binary format

--- Terr.cs
+++ Terr.cs
@@ -7,7 +7,6 @@ namespace HeightMapGenerator
 {
     public class Terr
     {
-        public int Size { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
 
@@ -43,11 +42,11 @@ namespace HeightMapGenerator
             string id = ASCIIEncoding.ASCII.GetString(reader.ReadBytes(4));
             if (id != "TERR") throw new IOException("Could not find TERR block");
 
-            Size = reader.ReadInt32();
+            reader.ReadInt32(); // Size (not needed)
             Width = reader.ReadInt32();
             Height = reader.ReadInt32();
 
-            reader.ReadInt32(); // Compressed block count (not needed)
+            int compressedBlockCount = reader.ReadInt32();
             int uncompressedBlockCount = reader.ReadInt32();
             reader.ReadInt32(); // Block count * 16? (purpose unknown)
 
@@ -83,9 +82,9 @@ namespace HeightMapGenerator
 
             int offsetCount = reader.ReadInt32();
 
-            if (offsetCount % 64 != 0)
+            if (compressedBlockCount * 64 != offsetCount)
             {
-                throw new IOException("Offset count not a multiple of 64");
+                throw new IOException("Compressed block count and offset count mismatch");
             }
 
             for (int i = 0; i < offsetCount / 64; ++i)
@@ -94,6 +93,57 @@ namespace HeightMapGenerator
             }
 
             // Should be end of TERR now...
+        }
+
+        /// <summary>
+        /// Converts Terr object into binary blob Terr format
+        /// </summary>
+        /// <returns></returns>
+        public byte[] ToArray()
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryWriter bw = new BinaryWriter(stream);
+                bw.Write(Encoding.ASCII.GetBytes("TERR"));
+                bw.Write(0); // Padding for Size
+                bw.Write(Width);
+                bw.Write(Height);
+
+                bw.Write(Offsets.Count); // Compressed Count
+                bw.Write(Blocks.Count); // Block count (Uncompressed Count)
+                bw.Write(Offsets.Count * 16); // FIXME: This is a guess :/
+
+                // 1st heightmap
+                foreach (Terrblock block in Blocks)
+                {
+                    bw.Write(block.Minimum);
+                    bw.Write(block.OffsetIndex * 64);
+                }
+                // 2nd heightmap
+                foreach (Terrblock block in Blocks_hmap2)
+                {
+                    bw.Write(block.Minimum);
+                    bw.Write(block.OffsetIndex * 64);
+                }
+
+                bw.Write(Offsets.Count * 64);
+
+                foreach (byte[] offsets in Offsets)
+                {
+                    bw.Write(offsets);
+                }
+
+                byte[] newHeightmap = stream.ToArray();
+
+                // Size is without TERR and size itself
+                int size = newHeightmap.Length - 8;
+                newHeightmap[4] = (byte)size;
+                newHeightmap[5] = (byte)(size >> 8);
+                newHeightmap[6] = (byte)(size >> 16);
+                newHeightmap[7] = (byte)(size >> 24);
+
+                return newHeightmap;
+            }
         }
     }
 
